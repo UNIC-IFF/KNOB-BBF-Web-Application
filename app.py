77261917a425afedb703c244d3222c3338fce0a0
George Michoulis <gmixoulis@gmail.com>
2022-07-27 18:19:41 +0300

    update app.py

--- app.py
+++ app.py
@@ -26,19 +26,6 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
     }
 )
 
-num2words1 = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', \
-            6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', \
-            11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', \
-            15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}
-num2words2 = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
-
-def number(Number):
-
-    if (Number > 1) or (Number < 19):
-        return (num2words1[Number])
-    elif (Number > 20) or (Number < 99):
-        return (num2words2[Number])
-
 def flatten(listOfLists):
     "Flatten one level of nesting"
     return chain.from_iterable(listOfLists)
@@ -125,6 +112,6 @@ if __name__ == '__main__':
                         help="Use flask debug/dev mode with file change reloading")
     ARGS = PARSER.parse_args()
 
-    PORT = int(os.environ.get('PORT', 80))
+    PORT = int(os.environ.get('PORT', 443))
 
     APP.run(host='0.0.0.0', port=443, debug=True, extra_files=['./static/swagger.json'])
